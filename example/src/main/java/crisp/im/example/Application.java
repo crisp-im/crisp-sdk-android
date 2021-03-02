fa5eab451e3e2855c2c9fa2450eccd0a747092ea
Sebastien Vitard <pro@sebastienvitard.com>
2021-03-02 19:00:07 +0100

    Bump 1.0.1

--- example/src/main/java/crisp/im/example/Application.java
+++ example/src/main/java/crisp/im/example/Application.java
@@ -11,6 +11,6 @@ public class Application extends android.app.Application {
     public void onCreate() {
         super.onCreate();
 
-        Crisp.configure("7598bf86-9ebb-46bc-8c61-be8929bbf93d");
+        Crisp.configure(getApplicationContext(), "7598bf86-9ebb-46bc-8c61-be8929bbf93d");
     }
 }
